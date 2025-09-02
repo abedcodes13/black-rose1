9a948eabb38bf15cf85b79b60de0ed471c6f5c53
abedcodes13 <114854246+abedscodes@users.noreply.github.com>
2025-09-03 04:10:45 +0930

    change

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,12 +1,12 @@
 ﻿import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import userRouter from './routes/userRoute.js'
-import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js'
+import connectDB from '../config/mongodb.js'
+import connectCloudinary from '../config/cloudinary.js'
+import userRouter from '../routes/userRoute.js'
+import productRouter from '../routes/productRoute.js'
+import cartRouter from '../routes/cartRoute.js'
+import orderRouter from '../routes/orderRoute.js'
 
 // App Config
 
